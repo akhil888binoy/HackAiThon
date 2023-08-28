31e579201ba7e88f19bec897bcc4799b8d172f17
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-08-28 22:17:27 +0530

    responsive

--- src/scenes/hackathonPage/index.jsx
+++ src/scenes/hackathonPage/index.jsx
@@ -8,7 +8,7 @@ import {Box ,
     CardBody, CardFooter,  
     Text , Button , 
     Divider, HStack, Input,
-    VStack, ButtonGroup, theme, Radio, RadioGroup } from '@chakra-ui/react'
+    VStack, ButtonGroup, theme, Radio, RadioGroup, AbsoluteCenter } from '@chakra-ui/react'
  import img1 from "../../assets/1.png"
  import "@fontsource/poppins";
  import { useEffect , useState} from 'react';
@@ -17,6 +17,16 @@ import {Box ,
  import img3 from "../../assets/3.png"
  import { Link } from 'react-router-dom';
 import HackathonDetail from '../hackathonDetail/HackathonDetailPage';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
+import { useDisclosure } from '@chakra-ui/react'
 const cardOptions={
   position : "absolute",
    left:"50%",
@@ -54,7 +64,9 @@ const hackathonfooterOptions={
 
 
 const HackathonPage = () => {
-    const [isOpen, setIsOpen] = React.useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+    const [isOpenn, setIsOpen] = React.useState(false);
    const [hackthonItems, setHackthonItems] = useState([]);
    useEffect(() => {
      setHackthonItems(hackathonData);
@@ -67,6 +79,7 @@ const HackathonPage = () => {
  
      const [isMobile] = useMediaQuery("(max-width: 768px)");
      const [showText, setShowText] = React.useState(true);
+     
  
    
   return (
@@ -74,12 +87,14 @@ const HackathonPage = () => {
     <Box w={"full"} h={"full"} >
       <Image src={img1} h={isMobile ? "1500px" : "full"} w={'full'} objectFit={'strech'}/>
       <ChakraProvider theme={theme}></ChakraProvider>
+
       
       <Container  minH={"100vh"}  backdropFilter='auto' backdropBlur='8px'
     maxW={"container.xl"} {...cardOptions}  top={isMobile ? "40%" : "40%"}
      >
+     
       <Center>
-      <Input variant='filled' placeholder='Search' maxW={"300px"} maxH={"100px"} mb={isMobile ? "15px" : ""} ml={isMobile ? "7px" : ""}/>
+      <Input variant='filled' placeholder='Search' maxW={"300px"} maxH={"100px"}  ml={isMobile ? "7px" : ""} mt={isMobile ? "7px" : "6rem"}/>
 
 
       </Center>
@@ -88,9 +103,145 @@ const HackathonPage = () => {
         alignItems={isMobile ? "center" : "flex-start"}
         flexWrap={isMobile ? "wrap" : "nowrap"}
         direction={["column", "row"]}>
+          //isMobile
+          {
+            isMobile&&(
+              <>
+            <Button ref={btnRef}  color='white' padding={ "5"} borderRadius={"25"} fontSize={isMobile ? "md" : "lg"} fontWeight={"bold"} width={ isMobile ? "10rem" : "25rem"}  h={"3rem"} bgColor="#6617cb"   ml={isMobile ? "9rem" : " "} mt={isMobile ? "2rem" : ""} mb={isMobile ? "3rem": ""}
+                             bgImage="linear-gradient(315deg, #6617cb 0%, #cb218e 74%)"  _hover={{ bg: "#c961de" }}
+                             sx={{
+                               "&:hover > *": {
+                                 color: "white",
+                               },
+                             }} onClick={onOpen}>
+        Filters
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent bgColor={"whiteAlpha.200"} backdropFilter='auto' backdropBlur='8px' >
+          <DrawerCloseButton />
+          <DrawerBody >
+          <Container className='SideButtons' mt="3rem"   >
+                                <VStack  >
+                                <Card width={"300px"} bgColor={""} >
+<CardBody>
+<Center>
+<Heading size='md' textColor={"white"}>Sort </Heading>
+</Center>
+
+<VStack mt='6' spacing='3'>
+
+<Button colorScheme='white' variant='ghost'>
+    Most Relevant 
+  </Button>
+  <Button colorScheme='white' variant='ghost'>
+   Submission Date
+  </Button>
+  <Button colorScheme='white' variant='ghost'>
+    Recently added
+  </Button>
+  <Button colorScheme='white' variant='ghost'>
+    Prize Amount
+  </Button>
+
+
+
+
+  <Card  bgColor={""} w={"300px"} mt={"1rem"} >
+<CardBody >
+<Center>
+<Heading size='md' mt={"4"}>Filters
+</Heading>
+
+</Center>
+
+
+<VStack mt='6' spacing='3'>
+  <Heading size='md'>Location</Heading>
+  <RadioGroup defaultValue='2'>
+  <Stack spacing={5} direction='column'>
+    <Radio  value='1'>
+     Offline
+    </Radio>
+    <Radio  value='2'>
+      Hybrid
+    </Radio>
+    <Radio  value='3'>
+      Online
+    </Radio>
+  </Stack>
+</RadioGroup>
+<Heading size={"md"}> Interested Tags</Heading>
+  <Stack direction={"row"}>
+  <Button colorScheme='teal' textColor="white" size='sm' borderRadius={"20px"}>
+    Tags
+  </Button>
+  <Button colorScheme='teal' textColor="white" size='sm' borderRadius={"20px"}>
+    Buttontag
+  </Button>
+  <Button colorScheme='teal' textColor="white" size='sm'  borderRadius={"20px"} >
+    Button
+  </Button>
+
+  </Stack>
+  <Stack direction={"row"}>
+  <Button colorScheme='teal' textColor="white" size='sm' borderRadius={"20px"} >
+    Button
+  </Button>
+  <Button colorScheme='teal' textColor="white" size='sm' borderRadius={"20px"}>
+    Button
+  </Button>
+  <Button colorScheme='teal' textColor="white" size='sm' borderRadius={"20px"}>
+    Button
+  </Button>
+
+  </Stack>
+  <Stack direction={"row"}>
+  <Button colorScheme='teal' textColor="white" size='sm' borderRadius={"20px"}>
+    Button
+  </Button>
+  <Button colorScheme='teal' textColor="white" size='sm' borderRadius={"20px"}>
+    Button
+  </Button>
+  <Button colorScheme='teal' textColor="white" size='sm' borderRadius={"20px"}>
+    Button
+  </Button>
+
+  </Stack>
+  
+
+</VStack>
+</CardBody>
+
+</Card>
+
+
+</VStack>
+</CardBody>
+</Card>
+
+                               
+
+                                </VStack>
+                            
+                            </Container>
+            
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+
+              
+              </>
+            )
+          }
 
  //Sorting           
-<Card width={"300px"} bgColor={"whiteAlpha.100"} ml={isMobile ? " 80px" : ""}>
+<Card width={"300px"} bgColor={"whiteAlpha.100"} display={isMobile ? "none" : ""}>
 <CardBody>
 <Center>
 <Heading size='md' textColor={"white"}>Sort </Heading>
@@ -122,7 +273,7 @@ const HackathonPage = () => {
 
 //Hackathons
 
-<Card  bgColor={"whiteAlpha.200"} width={"1000px"} className='HackathonsCard' mt={"25px"}>
+<Card  bgColor={"whiteAlpha.200"} width={"1000px"} className='HackathonsCard' mt={isMobile ? "" : "25px"} >
 <CardBody>
 <Center>
 <Heading size='lg'>Hackathons For You </Heading>
@@ -134,11 +285,13 @@ const HackathonPage = () => {
 direction={{ base: 'column', sm: 'row' }}
 overflow='hidden'
 variant='outline'
-maxW={"700px"} key={index}
+w={isMobile ? "400px" : "700px"} key={index}
+h={isMobile ? "15rem" : ""}
+
 >
 <Image
 objectFit='cover'
-maxW={isMobile ? "140px" : "200px"}
+w={isMobile ? "140px" : "200px"}
 src={img2}
 alt='Caffe Latte'
 />
@@ -148,20 +301,22 @@ alt='Caffe Latte'
 objectFit="cover"
 src={img3}
 alt='Caffe Latte'
+h={isMobile ? "20rem" : ""}
+w={isMobile ? "" : "50rem"}
 />
-<Text  fontSize={isMobile ? "10px" : "15px"} borderRadius={"10px"} {...locationOptions} bgColor={"teal"} textAlign={"center"}>
+<Text   fontSize={isMobile ? "15px" : "15px"}  borderRadius={"10px"} {...locationOptions} bgColor={"teal"} textAlign={"center"}>
     {hackathon.location}
 </Text>
 <CardBody {...hackathonOptions}  >
 
-<Heading size={isMobile ? "sm" : "md"}> {hackathon.NameOfHackathon}</Heading>
+<Heading size={isMobile ? "md" : "md"} > {hackathon.NameOfHackathon}</Heading>
     
  
     
-  <Text  my={isMobile ? "2" : "4"} px={isMobile ? "1" : "2"} fontWeight={"bold"} fontSize={isMobile ? "10px" : "15px"} borderRadius={"10px"} bgColor={"purple"}>
+  <Text  my={isMobile ? "2" : "4"} px={isMobile ? "5" : "2"}  fontWeight={"bold"} fontSize={isMobile ? "15px" : "15px"} borderRadius={"10px"} bgColor={"purple"}>
     {hackathon.date}
   </Text>
-  <Text  my={isMobile ? "1" : "4"}  mx={"2"} fontSize={isMobile ? "10px" : "15px"} >
+  <Text  my={isMobile ? "1" : "4"}  mx={"2"} fontSize={isMobile ? "15px" : "15px"} >
     {hackathon.ConductedBy}
   </Text>
     
@@ -169,14 +324,14 @@ alt='Caffe Latte'
 </CardBody>
 
 <CardFooter {...hackathonfooterOptions}>
-  <Button variant={"solid"} bgColor={'#c961de'} size={isMobile ? "sm" : "sm"} borderRadius={isMobile ? "20px" : "40px"}  ml={"5"}>
+  <Button variant={"solid"} bgColor={'#c961de'} size={isMobile ? "sm" : "lg"} borderRadius={isMobile ? "20px" : "40px"}  ml={"5"}>
     <Link to={"/hackathondetail"}>Join</Link>
     
   </Button >
-  <Button variant='solid' bgGradient={"linear-gradient(315deg, #facc6b 0%, #fabc3c 74%)"} mx={isMobile ? "4" : "9"} size={"sm"}>
+  <Button variant='solid' bgGradient={"linear-gradient(315deg, #facc6b 0%, #fabc3c 74%)"} mx={isMobile ? "4" : "9"} size={isMobile ? "sm" : "lg"}>
   $1000
   </Button>
-  <Button variant='solid'  bgColor={"#1c1c65"} size={"sm"}  mr="10">
+  <Button variant='solid'  bgColor={"#1c1c65"} size={isMobile ? "sm" : "lg"}  mr="10">
   {hackathon.NoOfHackers} hackers
   </Button>
   
@@ -193,7 +348,7 @@ alt='Caffe Latte'
 </Card>
 
 //Filters
-<Card  bgColor={"whiteAlpha.100"}w={"300px"} mx={isMobile ? "100" : ""}>
+<Card  bgColor={"whiteAlpha.100"}w={"300px"} display={isMobile ? "none" : ""}>
 <CardBody >
 <Center>
 <Heading size='md' mt={"4"}>Filters
